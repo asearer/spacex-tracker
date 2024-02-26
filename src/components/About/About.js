04a838eb9dd666fdf63d47094e0c7739ec9c4e14
asearer <amsearer84@gmail.com>
2024-02-26 18:41:24 -0500

    On branch main  Your branch is up to date with 'origin/main'.  Changes to be committed: 	modified:   src/components/About/About.js

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -8,7 +8,7 @@ function About() {
       <p className="about-text">Welcome to the SpaceX Launch Tracker!</p>
       <p className="about-text">This app is designed to help you track SpaceX launches, providing information on both past and upcoming launches, as well as resources and information to help keep you up to date regarding SpaceX missions!</p>
       <p className="about-explore">Let's explore...</p>
-      <p className="about-text">Stay up-to-date with SpaceX's missions and explore the exciting world of space exploration with us!</p>
+      <p className="about-text">Stay up-to-date with SpaceX's missions and explore the wonderful world of space exploration!</p>
     </div>
   );
 }
